33b8fce8528e9127a36f24c392973f4ff0c84bcc
vagrant <vagrant@homestead>
2015-05-26 14:34:01 +0000

    User account kapcsolat

--- app/User.php
+++ app/User.php
@@ -31,4 +31,8 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 */
 	protected $hidden = ['password', 'remember_token'];
 
+    public  function accounts(){
+        return $this->hasMany('App\Account');
+    }
+
 }
